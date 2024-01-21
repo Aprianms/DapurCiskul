01a448580cb7f6bf774ccf3f30adb4e7e34936ff
Aprianms <155547311+Aprianms@users.noreply.github.com>
2024-01-21 09:43:11 +0700

    Delete script-menu.js

--- script-menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Ambil nilai parameter "image" dari URL
-    const params = new URLSearchParams(window.location.search);
-    const imageName = params.get("image");
-    // Setel src gambar berdasarkan nilai parameter
-    let menuImage = document.getElementById("menuImg");
-    menuImage.src = "img/menu/" + imageName;
-  });
-  
